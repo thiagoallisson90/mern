73217fb37a1402b634232cf650999c5e6037ab41
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-11-26 03:39:18 -0300

    refactor: close connection

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import express from "express";
 import cors from "cors";
 import userRouter from "./src/routes/UserRoutes.js";
-import connectDatabase from "./src/database/db.js";
+import { createConnection } from "./src/database/db.js";
 
-const app = express();
-const port = 3000;
+(async () => {
+  const app = express();
+  const port = 3000;
 
-connectDatabase();
+  app.use(express.json());
+  app.use(cors());
+  app.use("/user", userRouter);
 
-app.use(express.json());
-app.use(cors());
-app.use("/user", userRouter);
+  console.log("Connecting to the database");
+  await createConnection();
 
-app.listen(port, () => {
-  console.log(`Server running in port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Server running in port ${port}`);
+  });
+})();
--- src/database/db.js
+++ src/database/db.js
@@ -15,4 +15,29 @@ const connectDatabase = () => {
     .catch((error) => console.log(error));
 };
 
-export default connectDatabase;
+const createConnection = async () => {
+  await mongoose.connect(
+    "mongodb+srv://thiagoallisson:HqlOhwFsjuDDZBbc@cluster0.egpjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
+};
+
+mongoose.connection.on("connected", () => {
+  console.log("Connection established");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error on connection", err);
+});
+
+process.on("SIGINT", async () => {
+  try {
+    await mongoose.connection.close();
+    console.log("Conection closed");
+    process.exit(0);
+  } catch (err) {
+    console.error("Error to close connection:", err);
+    process.exit(1);
+  }
+});
+
+export { connectDatabase, createConnection };
