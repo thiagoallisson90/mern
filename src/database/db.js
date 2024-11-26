1bdb5bb75d7e10c7f5fe2f4b85e2dce1006a5ef5
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-11-26 03:39:48 -0300

    refactor: remove code

--- src/database/db.js
+++ src/database/db.js
@@ -1,20 +1,5 @@
 import mongoose from "mongoose";
 
-const connectDatabase = () => {
-  console.log("Wait connecting to the database");
-
-  mongoose
-    .connect(
-      "mongodb+srv://thiagoallisson:HqlOhwFsjuDDZBbc@cluster0.egpjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" /*,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }*/
-    )
-    .then(() => console.log("Mongo DB Atlas Connected"))
-    .catch((error) => console.log(error));
-};
-
 const createConnection = async () => {
   await mongoose.connect(
     "mongodb+srv://thiagoallisson:HqlOhwFsjuDDZBbc@cluster0.egpjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
